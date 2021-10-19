e4f97b7e31b20e088ca3fdcfe9c284b3d72236f5
Roman Kniazev <kniazev.re@gmail.com>
2021-10-19 22:55:58 +0300

    chefs

--- app/js/main.js
+++ app/js/main.js
@@ -74,3 +74,18 @@ $(document).ready(function() {
         onBeforePrevSlide: function (el) {}
     });
 });
+
+// --- Chefs social links --- //
+$(document).ready(function(){
+    $('#Brady_Smith_card').click(function() {
+        $('#Brady_Smith_socials').slideToggle();
+    });
+
+    $('#Tom_Eddison_card').click(function() {
+        $('#Tom_Eddison_socials').slideToggle();
+    });
+
+    $('#Willy_Chan_card').click(function() {
+        $('#Willy_Chan_socials').slideToggle();
+    });
+});
