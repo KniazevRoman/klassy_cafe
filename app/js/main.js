6fa1b8a224e59b312dbd4843260f5b62b7d26824
Roman Kniazev <kniazev.re@gmail.com>
2021-10-28 22:30:10 +0300

    specials

--- app/js/main.js
+++ app/js/main.js
@@ -89,3 +89,146 @@ $(document).ready(function(){
         $('#Willy_Chan_socials').slideToggle();
     });
 });
+
+// Special menu options //
+
+const specialMenu = [
+    {
+        pic: 'imgs/tab-item-01.png',
+        title: 'Veggy salad',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, sit.',
+        price: '$10'
+    },
+    {
+        pic: 'imgs/tab-item-02.png',
+        title: 'Fresh juice',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, sit.',
+        price: '$4'
+    },
+    {
+        pic: 'imgs/tab-item-03.png',
+        title: 'Chiken salad',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, sit.',
+        price: '$18'
+    },
+    {
+        pic: 'imgs/tab-item-04.png',
+        title: 'Omelette',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, sit.',
+        price: '$4'
+    },
+    {
+        pic: 'imgs/tab-item-05.png',
+        title: 'Pumpkin soup',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, sit.',
+        price: '$5'
+    },
+    {
+        pic: 'imgs/tab-item-06.png',
+        title: 'Omelette&Cheese',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, sit.',
+        price: '$12'
+    },
+    {
+        pic: 'imgs/tab-item-04.png',
+        title: 'Omelette',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, sit.',
+        price: '$4'
+    },
+    {
+        pic: 'imgs/tab-item-02.png',
+        title: 'Fresh juice',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, sit.',
+        price: '$4'
+    },
+    {
+        pic: 'imgs/tab-item-01.png',
+        title: 'Veggy salad',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, sit.',
+        price: '$10'
+    },
+    {
+        pic: 'imgs/tab-item-03.png',
+        title: 'Chiken salad',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, sit.',
+        price: '$18'
+    },
+    {
+        pic: 'imgs/tab-item-05.png',
+        title: 'Pumpkin soup',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, sit.',
+        price: '$5'
+    },
+];
+
+function createColumn () {
+    const column = `
+        <div class="col-12 col-md-6"></div>
+    `;
+
+    return column;
+}
+
+function createOptionCard (pic, title, desc, price) {
+    const optionCard = `
+        <div class="card mb-3 border-0">
+            <div class="row g-0">
+                <div class="col-4 d-flex justify-content-center align-items-center">
+                    <img src="${pic}" class="img-fluid" alt="${title}">
+                </div>
+                <div class="col-6">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <p class="card-text">${desc}</p>
+                    </div>
+                </div>
+                <div class="col-2 d-flex justify-content-center align-items-center">
+                    <h3 class="text-light m-0 bg-primary p-4 border rounded">${price}</h3>
+                </div>
+            </div>
+        </div>
+    `;
+
+    return optionCard;
+}
+
+function createMenu(start) {
+    $('#options-container').html('');
+    for (let i = 0; i <= 1; i++) {
+        $('#options-container').append(createColumn());
+
+        if (i == 0) {
+            for (let j = start; j < (start + 3); j++) {
+                let {pic, title, desc, price} = specialMenu[j];
+
+                const optionCard = createOptionCard (pic, title, desc, price);
+
+                $('#options-container>div:first-child').append(optionCard);
+            }
+        } else {
+            for (let j = (start + 3); j < (start + 6); j++) {
+                let {pic, title, desc, price} = specialMenu[j];
+
+                const optionCard = createOptionCard (pic, title, desc, price);
+
+                $('#options-container>div:last-child').append(optionCard);
+            }
+        }
+    }
+}
+
+$('#breakfast-special').click(function() {
+    createMenu(0)
+});
+
+$('#lunch-special').click(function() {
+    createMenu(3)
+});
+
+$('#dinner-special').click(function() {
+    createMenu(5)
+});
+
+$(document).ready(function(){
+    $('#breakfast-special').trigger( "click" );
+});
